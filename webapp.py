646480407fcc1080c46b053c4acfbaa336208aca
camsqu <camsqu@gmail.com>
2017-11-28 09:30:02 -0800

    about

--- webapp.py
+++ webapp.py
@@ -7,3 +7,7 @@ app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  
 @app.route("/")
 def render_main():
     return render_template('index.html')
+
+@app.route("/about")
+def render_about():
+    return render_template('about.html')
