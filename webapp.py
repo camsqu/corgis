dff80ba6609294d4e50c04e2477e3ef6f67bc7f5
camsqu <camsqu@gmail.com>
2017-11-30 09:26:14 -0800

    link json to webapp and overall data start

--- webapp.py
+++ webapp.py
@@ -4,6 +4,10 @@ import json
 
 app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  Otherwise, it is the name of the file (ex. webapp)
 
+def main():
+    with open('immigration.json') as immigration_data:
+        countries = json.load(immigration_data)
+
 @app.route("/")
 def render_main():
     return render_template('index.html')
@@ -15,3 +19,7 @@ def render_about():
 @app.route("/overall")
 def render_overall():
     return render_template('overall.html')
+
+def number_of_countries():
+    countryTotal = {}
+    country = countries[0]['Country']
