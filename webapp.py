7fd918a2964893dfd6c6453d53949ed2b97189ed
camsqu <camsqu@gmail.com>
2017-12-01 09:56:39 -0800

    line break test

--- webapp.py
+++ webapp.py
@@ -49,7 +49,9 @@ def country_response(country):
         countries = json.load(immigration_data)
     for z in countries:
         if z["Country"] == country:
-            return "The immigration data for " + z["Country"] + " is " + str(z["Data"]["Enforcement"])
+            return "Immigration data for " + z["Country"] /
+            + "Enforcement:" /
+            + "Non-criminal:" + str(z["Data"]["Enforcement"])
 @app.route("/response")
 def render_response():
     country = request.args['Country']
